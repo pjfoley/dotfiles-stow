f27e94f76385ece08a6deb744eb7f1a272a2daa7
Peter Foley <peter@ifoley.id.au>
2017-04-11 21:54:42 +1000

    Add install script for vim dependancies

---
+++ vim/.config/dot_install/vim.sh
@@ -0,0 +1,13 @@
+#! /bin/sh
+
+PACKAGES=("vim-nox python-pip")
+
+for package in "${PACKAGES}"; do
+    dpkg -s "${package}" >/dev/null 2>&1 && {
+        echo "${package} is installed."
+    } || {
+        sudo apt-get install -y "${package}"
+    }
+done
+
+sudo pip install powerline-status

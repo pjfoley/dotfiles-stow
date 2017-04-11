c949031546eabf1c8ba4673994bc6e3084594ed0
Peter Foley <peter@ifoley.id.au>
2017-04-11 22:51:27 +1000

    Add powerline package installation

--- vim/.config/dot_install/vim.sh
+++ vim/.config/dot_install/vim.sh
@@ -1,13 +1,11 @@
-#! /bin/sh
+#! /bin/ash
 
-PACKAGES=("vim-nox python-pip")
+PACKAGES=( "vim-nox" "python-pip" "powerline" " fonts-powerline")
 
-for package in "${PACKAGES}"; do
+for package in "${PACKAGES[@]}"; do
     dpkg -s "${package}" >/dev/null 2>&1 && {
         echo "${package} is installed."
     } || {
         sudo apt-get install -y "${package}"
     }
 done
-
-sudo pip install powerline-status

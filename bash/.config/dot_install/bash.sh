693e52287079063296f37c4a234c9883800767fc
Peter Foley <peter@ifoley.id.au>
2017-04-15 14:23:04 +1000

    Install bash dependancies

---
+++ bash/.config/dot_install/bash.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+PACKAGES=( "python-pip" "powerline" "fonts-powerline")
+
+for package in "${PACKAGES[@]}"; do
+    dpkg -s "${package}" >/dev/null 2>&1 && {
+        echo "${package} is installed."
+    } || {
+        sudo apt-get install -y "${package}"
+    }
+done

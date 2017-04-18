170b4d4a6e55f6a3547447b8a6a0fea2158a6409
Peter Foley <peter@ifoley.id.au>
2017-04-18 22:16:29 +1000

    Add install console app

--- i3wm/.config/dot_install/i3wm.sh
+++ i3wm/.config/dot_install/i3wm.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-PACKAGES=( "i3" "i3blocks" "i3status" "i3lock" "rofi" "lightdm" "suckless-tools")
+PACKAGES=( "i3" "i3blocks" "i3status" "i3lock" "rofi" "lightdm" "suckless-tools" "rxvt-unicode-256color")
 
 for package in "${PACKAGES[@]}"; do
     dpkg -s "${package}" >/dev/null 2>&1 && {

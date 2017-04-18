b815202c37edbb1126e6da516ed420e7785ab1be
Peter Foley <peter@ifoley.id.au>
2017-04-18 22:07:17 +1000

    Add i3 package dependancy install script

---
+++ i3wm/.config/dot_install/i3wm.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+PACKAGES=( "i3" "i3blocks" "i3status" "i3lock" "rofi" "lightdm" "suckless-tools")
+
+for package in "${PACKAGES[@]}"; do
+    dpkg -s "${package}" >/dev/null 2>&1 && {
+        echo "${package} is installed."
+    } || {
+        sudo apt-get install -y "${package}"
+    }
+done

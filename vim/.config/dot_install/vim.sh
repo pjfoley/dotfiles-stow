da8168f2c23cfee27ab21e1b8f9d0b6b75942725
Peter Foley <peter@ifoley.id.au>
2017-05-08 12:42:58 +1000

    Install YouCompleteMe dependancies

--- vim/.config/dot_install/vim.sh
+++ vim/.config/dot_install/vim.sh
@@ -2,6 +2,9 @@
 
 PACKAGES=( "vim-nox" "python-pip" "powerline" "fonts-powerline")
 
+# Youcompleteme dependancies
+PACKAGES+=( "build-essential" "cmake" "python-dev" "python3-dev")
+
 for package in "${PACKAGES[@]}"; do
     dpkg -s "${package}" >/dev/null 2>&1 && {
         echo "${package} is installed."

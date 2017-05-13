e9682729200e9a16f687f292c4b19bc3cc4d83f8
Peter Foley <peter@ifoley.id.au>
2017-05-13 13:03:16 +1000

    Add curl as a vim dependancy

--- vim/.config/dot_install/vim.sh
+++ vim/.config/dot_install/vim.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-PACKAGES=( "vim-nox" "python-pip" "powerline" "fonts-powerline")
+PACKAGES=( "vim-nox" "python-pip" "powerline" "fonts-powerline" "curl")
 
 # Youcompleteme dependancies
 PACKAGES+=( "build-essential" "cmake" "python-dev" "python3-dev")

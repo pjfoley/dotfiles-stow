ad04838d27554854b295279a859c257bbf7b17e5
Peter Foley <pjfoley@supanova.ifoley.local>
2017-06-04 00:10:30 +1000

    Migrate to using i3-sensible-terminal

--- i3wm/.config/i3/layouts/dev.sh
+++ i3wm/.config/i3/layouts/dev.sh
@@ -2,8 +2,8 @@
 i3-msg "workspace dev; append_layout ~/.config/i3/layouts/dev.json"
 
 i3-msg "workspace dev; \
-        exec urxvt -cd ~; \
-        exec urxvt -cd ~"
+        exec i3-sensible-terminal -d ~; \
+        exec i3-sensible-terminal -d ~"
 sleep .5s
 i3-msg "workspace dev; \
   exec google-chrome-stable --new-window chrome://newtab"
--- i3wm/.config/i3/layouts/media.sh
+++ i3wm/.config/i3/layouts/media.sh
@@ -2,16 +2,16 @@
 i3-msg "workspace media; append_layout ~/.config/i3/layouts/media.json"
 
 i3-msg "workspace media; \
-        exec urxvt -cd ~; \
-        exec urxvt -cd ~; \
-        exec urxvt -cd ~; \
-        exec urxvt -cd ~"
+        exec i3-sensible-terminal -d ~; \
+        exec i3-sensible-terminal -d ~; \
+        exec i3-sensible-terminal -d ~; \
+        exec i3-sensible-terminal -d ~"
 
 sleep .5s
 
 i3-msg "workspace media; \
   exec google-chrome-stable --new-window chrome://newtab; \
-  exec urxvt -title "Playlist" -e ncmpcpp -s playlist &"
+  exec i3-sensible-terminal --title "Playlist" -e ncmpcpp -s playlist &"
 
 # (urxvt -cd ~ &)
 # (urxvt -cd ~ &)

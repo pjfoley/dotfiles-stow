85063974e6d8f7fa7e1251aa57d9c203413c3455
Peter Foley <peter@ifoley.id.au>
2017-08-08 23:39:12 +1000

    Add some cli utility capabilities

--- bash/.config/dot_install/bash.sh
+++ bash/.config/dot_install/bash.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-PACKAGES=("bash-completion" "python-pip" "powerline" "fonts-powerline" "curl" "wget")
+PACKAGES=("bash-completion" "python-pip" "powerline" "fonts-powerline" "curl" "wget" "man" "jq" "bc")
 
 for package in "${PACKAGES[@]}"; do
     dpkg -s "${package}" >/dev/null 2>&1 && {
